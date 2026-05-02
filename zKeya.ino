129c0ca8c1dfc2aa0ce41f9275796aeb49b4503c
lansalot <lansalot@gmail.com>
2026-05-02 22:41:29 +0100

    Formatting, ugh

--- zKeya.ino
+++ zKeya.ino
@@ -1,44 +1,47 @@
 
 // templates for commands with matching responses, only need first 4 bytes
-uint8_t keyaDisableCommand[] = { 0x23, 0x0C, 0x20, 0x01 };
-uint8_t keyaDisableResponse[] = { 0x60, 0x0C, 0x20, 0x00 };
+// uint8_t keyaDisableCommand[] = { 0x23, 0x0C, 0x20, 0x01 }; // shouln't that be 0x11 inastead of 0x01??
+uint8_t keyaDisableCommand[] = {0x23, 0x0C, 0x20, 0x01}; // Drynie
+uint8_t keyaDisableResponse[] = {0x60, 0x0C, 0x20, 0x00};
 
-uint8_t keyaEnableCommand[] = { 0x23, 0x0D, 0x20, 0x01, 0x01, 0x90, 0x00, 0x00 };
-uint8_t keyaEnableResponse[] = { 0x60, 0x0D, 0x20, 0x00 };
+uint8_t keyaEnableCommand[] = {0x23, 0x0D, 0x20, 0x01, 0x01, 0x90, 0x00, 0x00};
+uint8_t keyaEnableResponse[] = {0x60, 0x0D, 0x20, 0x00};
 
-uint8_t keyaSpeedCommand[] = { 0x23, 0x00, 0x20, 0x01 };
-uint8_t keyaSpeedResponse[] = { 0x60, 0x00, 0x20, 0x00 };
+uint8_t keyaSpeedCommand[] = {0x23, 0x00, 0x20, 0x01};
+uint8_t keyaSpeedResponse[] = {0x60, 0x00, 0x20, 0x00};
 
-uint8_t keyaCurrentQuery[] = { 0x40, 0x00, 0x21, 0x01 };
-uint8_t keyaCurrentResponse[] = { 0x60, 0x00, 0x21, 0x01 };
+uint8_t keyaCurrentQuery[] = {0x40, 0x00, 0x21, 0x01};
+uint8_t keyaCurrentResponse[] = {0x60, 0x00, 0x21, 0x01};
 
-uint8_t keyaFaultQuery[] = { 0x40, 0x12, 0x21, 0x01 };
-uint8_t keyaFaultResponse[] = { 0x60, 0x12, 0x21, 0x01 };
+uint8_t keyaFaultQuery[] = {0x40, 0x12, 0x21, 0x01};
+uint8_t keyaFaultResponse[] = {0x60, 0x12, 0x21, 0x01};
 
-uint8_t keyaVoltageQuery[] = { 0x40, 0x0D, 0x21, 0x02 };
-uint8_t keyaVoltageResponse[] = { 0x60, 0x0D, 0x21, 0x02 };
+uint8_t keyaVoltageQuery[] = {0x40, 0x0D, 0x21, 0x02};
+uint8_t keyaVoltageResponse[] = {0x60, 0x0D, 0x21, 0x02};
 
-uint8_t keyaTemperatureQuery[] = { 0x40, 0x0F, 0x21, 0x01 };
-uint8_t keyaTemperatureResponse[] = { 0x60, 0x0F, 0x21, 0x01 };
+uint8_t keyaTemperatureQuery[] = {0x40, 0x0F, 0x21, 0x01};
+uint8_t keyaTemperatureResponse[] = {0x60, 0x0F, 0x21, 0x01};
 
-uint8_t keyaVersionQuery[] = { 0x40, 0x01, 0x11, 0x11 };
-uint8_t keyaVersionResponse[] = { 0x60, 0x01, 0x11, 0x11 };
+uint8_t keyaVersionQuery[] = {0x40, 0x01, 0x11, 0x11};
+uint8_t keyaVersionResponse[] = {0x60, 0x01, 0x11, 0x11};
 
-uint8_t keyaEncoderResponse[] = { 0x60, 0x04, 0x21, 0x01 };
+uint8_t keyaEncoderResponse[] = {0x60, 0x04, 0x21, 0x01};
 
-uint8_t keyaEncoderSpeedResponse[] = { 0x60, 0x03, 0x21, 0x01 };
+uint8_t keyaEncoderSpeedResponse[] = {0x60, 0x03, 0x21, 0x01};
 
 uint64_t KeyaID = 0x06000001; // 0x01 is default ID
 
 uint64_t keyaConfigID = 0x06000591;
-uint8_t keyaEnterConfig[] = { 0xFA, 0xFA, 0x00, 0x00 };
-uint8_t keyaSet5Amp[] = { 0xBB, 0xBB, 0x00, 0x00, 0x00, 0x03, 0x00, 0x05 };
-uint8_t keyaStoreEEPROM[] = { 0xFA, 0xFA, 0x00, 0x08 };
-uint8_t keyaExitConfig[] = { 0xFA, 0xFA, 0x00, 0xAA };
+uint8_t keyaEnterConfig[] = {0xFA, 0xFA, 0x00, 0x00};
+uint8_t keyaSet5Amp[] = {0xBB, 0xBB, 0x00, 0x00, 0x00, 0x03, 0x00, 0x20};
+uint8_t keyaStoreEEPROM[] = {0xFA, 0xFA, 0x00, 0x08};
+uint8_t keyaExitConfig[] = {0xFA, 0xFA, 0x00, 0xAA};
+uint8_t is5Amp[] = { 0xAA, 0xAA, 0x00, 0x03, 0x00, 0x02, 0x00 }; // this goes round reading  0 (parameter), 1 (ram) and 2 (read) so it takes a while as 2/read is last
 
+bool inConfig = false;
 bool keyaDebug = false;
 template <size_t N>
-void keyaConfig(uint8_t(&command)[N])
+void keyaConfig(uint8_t (&command)[N])
 {
 	if (keyaDetected)
 	{
@@ -48,7 +51,6 @@ void keyaConfig(uint8_t(&command)[N])
 		KeyaBusSendData.len = N;
 		memcpy(KeyaBusSendData.buf, command, N);
 		canbus3.write(KeyaBusSendData);
-		Serial.println("Keya configuration command sent.");
 		delay(2000);
 	}
 }
@@ -69,22 +71,44 @@ void KeyaBus_Receive()
 	CAN_message_t KeyaBusReceiveData;
 	if (canbus3.read(KeyaBusReceiveData))
 	{
+		// Config messages
+		if (KeyaBusReceiveData.id == 0x181)
+		{
+			Serial.print(".");
+			if (memcmp(KeyaBusReceiveData.buf, is5Amp, 7) == 0 && KeyaBusReceiveData.buf[7] == 0x05)
+			{
+				Serial.println("\nKeya reports 5amp cutoff configured");
+				Serial.println("Exiting config mode");
+				keyaConfig(keyaExitConfig);
+				inConfig = false;
+			}
+			else if (memcmp(KeyaBusReceiveData.buf, is5Amp, 7) == 0 && KeyaBusReceiveData.buf[7] != 0x05)
+			{
+				Serial.println("\nKeya reports 5amp cutoff NOT configured");
+				Serial.println("Set 5amp cutoff");
+				keyaConfig(keyaSet5Amp);
+				keyaConfig(keyaStoreEEPROM);
+				Serial.println("Exiting config mode");
+				keyaConfig(keyaExitConfig);
+				inConfig = false;
+			}
+		}
 		// Heatbeat
 		if (KeyaBusReceiveData.id == 0x07000001)
 		{
 			lastKeyaHeatbeat = 0;
 
-            if (!keyaDetected)
-            {
-                Serial.println("Keya heartbeat detected! Enabling Keya CANBUS and setting 5 amp max current");
-                keyaDetected = true;
-                keyaConfig(keyaEnterConfig);
-                keyaConfig(keyaSet5Amp);
+			if (!keyaDetected)
+			{
+				Serial.println("Keya heartbeat detected! Enabling Keya CANBUS and setting 5 amp max current");
+				keyaDetected = true;
+				keyaConfig(keyaEnterConfig);
+				keyaConfig(keyaSet5Amp);
 				// try without storing in eeprom, save writes?
-                keyaConfig(keyaStoreEEPROM);
-                keyaConfig(keyaExitConfig);
+				keyaConfig(keyaStoreEEPROM);
+				keyaConfig(keyaExitConfig);
 				Serial.println("Set to 5amp cutoff");
-            }
+			}
 			// 0-1 - Cumulative value of angle (360 def / circle)
 			// 2-3 - Motor speed, signed int eg -500 or 500
 			// 4-5 - Motor current
@@ -102,21 +126,22 @@ void KeyaBus_Receive()
 			{
 				if (counter++ < 8)
 				{
-					//Serial.print("Counter\t");
+					// Serial.print("Counter\t");
 				}
 				else
 				{
-					//Serial.print("Stop\t");
+					// Serial.print("Stop\t");
 					sensorReading = abs(abs(keyaCurrentSetSpeed) - error);
 				}
 			}
 			else
 			{
-				//Serial.print("Run\t");
+				// Serial.print("Run\t");
 				sensorReading = 0;
 				counter = 0;
 			}
-			if (keyaDebug) {
+			if (keyaDebug)
+			{
 
 				Serial.print("steeringPosition: ");
 				Serial.print(keyaSteeringPosition);
@@ -126,7 +151,6 @@ void KeyaBus_Receive()
 				Serial.print(current);
 				Serial.print("\tCurrentSetSpeed: ");
 
-
 				Serial.print(keyaCurrentSetSpeed);
 				Serial.print("\tCurrentActualSpeed: ");
 				Serial.print(keyaCurrentActualSpeed);
@@ -134,30 +158,47 @@ void KeyaBus_Receive()
 				Serial.print(error);
 				Serial.print("\t");
 
-
-				if (bitRead(KeyaBusReceiveData.buf[7], 0)) Serial.println("Disabled\t");
-				else Serial.println("Enabled \t");
+				if (bitRead(KeyaBusReceiveData.buf[7], 0))
+					Serial.println("Disabled\t");
+				else
+					Serial.println("Enabled \t");
 			}
 			// check if there's any motor diag/error data and parse it
 			if (KeyaBusReceiveData.buf[7] > 1 || KeyaBusReceiveData.buf[6] > 0)
 			{
-				if (keyaDebug) {
-					if (bitRead(KeyaBusReceiveData.buf[7], 1)) Serial.print("Over voltage\t");
-					if (bitRead(KeyaBusReceiveData.buf[7], 2)) Serial.print("Hardware protection\t");
-					if (bitRead(KeyaBusReceiveData.buf[7], 3)) Serial.print("E2PROM\t");
-					if (bitRead(KeyaBusReceiveData.buf[7], 4)) Serial.print("Under voltage\t");
-					if (bitRead(KeyaBusReceiveData.buf[7], 5)) Serial.print("N/A\t");
-					if (bitRead(KeyaBusReceiveData.buf[7], 6)) Serial.print("Over current\t");
-					if (bitRead(KeyaBusReceiveData.buf[7], 7)) Serial.print("Mode failure\t");
+				if (keyaDebug)
+				{
+					if (bitRead(KeyaBusReceiveData.buf[7], 1))
+						Serial.print("Over voltage\t");
+					if (bitRead(KeyaBusReceiveData.buf[7], 2))
+						Serial.print("Hardware protection\t");
+					if (bitRead(KeyaBusReceiveData.buf[7], 3))
+						Serial.print("E2PROM\t");
+					if (bitRead(KeyaBusReceiveData.buf[7], 4))
+						Serial.print("Under voltage\t");
+					if (bitRead(KeyaBusReceiveData.buf[7], 5))
+						Serial.print("N/A\t");
+					if (bitRead(KeyaBusReceiveData.buf[7], 6))
+						Serial.print("Over current\t");
+					if (bitRead(KeyaBusReceiveData.buf[7], 7))
+						Serial.print("Mode failure\t");
 
-					if (bitRead(KeyaBusReceiveData.buf[6], 0)) Serial.print("Less phase\t");
-					if (bitRead(KeyaBusReceiveData.buf[6], 1)) Serial.print("Motor stall\t");
-					if (bitRead(KeyaBusReceiveData.buf[6], 2)) Serial.print("Reserved\t");
-					if (bitRead(KeyaBusReceiveData.buf[6], 3)) Serial.print("Hall failure\t");
-					if (bitRead(KeyaBusReceiveData.buf[6], 4)) Serial.print("Current sensing\t");
-					if (bitRead(KeyaBusReceiveData.buf[6], 5)) Serial.print("No RS232 Steer Command\t");
-					if (bitRead(KeyaBusReceiveData.buf[6], 6)) Serial.print("No CAN Steer Command\t");
-					if (bitRead(KeyaBusReceiveData.buf[6], 7)) Serial.print("Motor stalled\t");
+					if (bitRead(KeyaBusReceiveData.buf[6], 0))
+						Serial.print("Less phase\t");
+					if (bitRead(KeyaBusReceiveData.buf[6], 1))
+						Serial.print("Motor stall\t");
+					if (bitRead(KeyaBusReceiveData.buf[6], 2))
+						Serial.print("Reserved\t");
+					if (bitRead(KeyaBusReceiveData.buf[6], 3))
+						Serial.print("Hall failure\t");
+					if (bitRead(KeyaBusReceiveData.buf[6], 4))
+						Serial.print("Current sensing\t");
+					if (bitRead(KeyaBusReceiveData.buf[6], 5))
+						Serial.print("No RS232 Steer Command\t");
+					if (bitRead(KeyaBusReceiveData.buf[6], 6))
+						Serial.print("No CAN Steer Command\t");
+					if (bitRead(KeyaBusReceiveData.buf[6], 7))
+						Serial.print("Motor stalled\t");
 
 					Serial.println("Kill Autosteer");
 				}
@@ -166,7 +207,7 @@ void KeyaBus_Receive()
 				previous = 0;
 			}
 
-			//Serial.println();
+			// Serial.println();
 		}
 	}
 }
@@ -175,6 +216,7 @@ void SteerKeya(int steerSpeed, bool intendToSteer)
 {
 	if (keyaDetected)
 	{
+		int actualScaledSpeed;
 		int16_t actualSpeed;
 		if (intendToSteer)
 		{
@@ -186,32 +228,35 @@ void SteerKeya(int steerSpeed, bool intendToSteer)
 			actualSpeed = 0;
 		}
 
-		keyaCurrentSetSpeed = actualSpeed * 0.1;
+		actualScaledSpeed = map(actualSpeed, -255, 255, -9995, 9998);
+		keyaCurrentSetSpeed = actualScaledSpeed * 0.1;
 
 		CAN_message_t KeyaBusSendData;
 		KeyaBusSendData.id = KeyaID;
 		KeyaBusSendData.flags.extended = true;
 		KeyaBusSendData.len = 8;
 		memcpy(KeyaBusSendData.buf, keyaSpeedCommand, 4);
-		if (intendToSteer) {
+		if (intendToSteer)
+		{
 			if (steerSpeed < 0)
 			{
-				KeyaBusSendData.buf[4] = highByte(actualSpeed);
-				KeyaBusSendData.buf[5] = lowByte(actualSpeed);
-				KeyaBusSendData.buf[6] = 0xff;
-				KeyaBusSendData.buf[7] = 0xff;
+				KeyaBusSendData.buf[6] = highByte(actualScaledSpeed);
+				KeyaBusSendData.buf[7] = lowByte(actualScaledSpeed);
+				KeyaBusSendData.buf[4] = 0xff;
+				KeyaBusSendData.buf[5] = 0xff;
 			}
 			else
 			{
-				KeyaBusSendData.buf[4] = highByte(actualSpeed);
-				KeyaBusSendData.buf[5] = lowByte(actualSpeed);
-				KeyaBusSendData.buf[6] = 0x00;
-				KeyaBusSendData.buf[7] = 0x00;
+				KeyaBusSendData.buf[6] = highByte(actualScaledSpeed);
+				KeyaBusSendData.buf[7] = lowByte(actualScaledSpeed);
+				KeyaBusSendData.buf[4] = 0x00;
+				KeyaBusSendData.buf[5] = 0x00;
 			}
 			canbus3.write(KeyaBusSendData);
 		}
 
-		if (intendToSteer) {
+		if (intendToSteer)
+		{
 			keyaCommand(keyaEnableCommand);
 		}
 	}
